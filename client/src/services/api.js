84c3796a823719b999d0aa4fdf30e4cba75933ff
Larissa Martins Mauricio <seu-email-de-login-no-larissa.mauricio@asaas.com.br>
2024-02-12 12:03:56 -0300

    solving routes error to load the page

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', //move to .env
+  baseURL: 'http://localhost:5000', //TODO: move to .env
 });
 
 export default api;

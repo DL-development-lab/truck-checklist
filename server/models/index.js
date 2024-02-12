fcd2af2c39fefd33383e18ccedbe67ceddb0378f
Larissa Martins Mauricio <seu-email-de-login-no-larissa.mauricio@asaas.com.br>
2024-02-12 11:51:04 -0300

    solving database error

--- server/models/index.js
+++ server/models/index.js
@@ -9,12 +9,15 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME || config.database,
+  process.env.DB_USER || config.username,
+  process.env.DB_PASSWORD || config.password,
+  {
+    host: process.env.DB_HOST || config.host,
+    dialect: 'mysql',
+  }
+);
 
 fs
   .readdirSync(__dirname)
